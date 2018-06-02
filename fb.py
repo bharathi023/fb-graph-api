66864e91062315a3abcab5ebf6f9c9edc1f622ef
Putta23 <31534695+bharathi023@users.noreply.github.com>
2018-06-02 10:59:17 +0530

    Add files via upload

---
+++ fb.py
@@ -0,0 +1,6 @@
+import requests
+import facebook
+token = ''#you facebook access token
+graph = facebook.GraphAPI(access_token=token,version='2.5')#here we are connecting to our account with access token using fb graph api
+user = graph.get_object('me',fields='name,id,email,education,birthday')
+print(user)
