f741f1a1b640be12df8605240a88e95fdf0391df
Putta23 <31534695+bharathi023@users.noreply.github.com>
2018-06-02 05:54:26 +0000

    Update fb.py

--- fb.py
+++ fb.py
@@ -2,5 +2,5 @@ import requests
 import facebook
 token = ''#you facebook access token
 graph = facebook.GraphAPI(access_token=token,version='2.5')#here we are connecting to our account with access token using fb graph api
-user = graph.get_object('me',fields='name,id,email,education,birthday')
+user = graph.get_object('me',fields='name,id,email,education,birthday,friends')
 print(user)
